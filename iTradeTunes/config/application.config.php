837ee32deb9145fce6f87730c3e382c38c658267
pwansch <pwansch@me.com>
2013-01-15 14:00:55 -0800

    Refactor application module.

--- iTradeTunes/config/application.config.php
+++ iTradeTunes/config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     'modules' => array(
         'Application',
-    	'Trade',
+    	'Trade', // iTradeTunes specific module
     	'AkrabatSession',
     ),
     'module_listener_options' => array(
